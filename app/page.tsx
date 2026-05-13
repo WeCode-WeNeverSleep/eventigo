9a05c309d3d85f7c382b714368f6ad3256e83616
mayahNeko <mayahibela@gmail.com>
2026-05-13 18:19:00 +0300

    feat: add SpeakerComponent for Speaker section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import { ModeToggle } from "@/components/theme-toggle";
 import QuestionForm from "@/components/question-component/QuestionForm";
+import SpeakerComponent from "@/components/SpeakerComponent/SpeakerComponent";
+
 export default function Home() {
   return (
     <>
       <ModeToggle />
-        <QuestionForm/>
+        <div className="flex flex-col md:flex-row gap-6">
+            <QuestionForm/>
+            <SpeakerComponent/>
+        </div>
     </>
   );
 }
