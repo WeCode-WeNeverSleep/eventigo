9e180746405fc2e68c58b438759b402e1fa32dbd
Rindraniaina Rasolofondrasoa <rindraniainarasolofondrasoa17@gmail.com>
2026-05-15 13:38:31 +0300

    reset page.tsx into default

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,9 @@
 import { ModeToggle } from "@/components/theme-toggle";
-import QuestionForm from "@/components/question-component/QuestionForm";
-import SpeakerComponent from "@/components/SpeakerComponent/SpeakerComponent";
 
 export default function Home() {
   return (
     <>
       <ModeToggle />
-      <div className="flex flex-col md:flex-row px-5">
-        <div className="md:w-2/3 w-full">
-          <QuestionForm />
-        </div>
-
-        <div className="md:w-1/3 w-full">
-          <SpeakerComponent />
-        </div>
-      </div>
     </>
   );
 }
